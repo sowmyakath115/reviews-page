a9e83e84a43745690c7df9d5242c5fcfdc517321
sowmya <sowmyakathari888@gmail.com>
2024-08-24 07:38:38 +0000

    answer

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,80 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {activeReview: 0}
+
+  onClickRightArrow = () => {
+    const {activeReview} = this.state
+    const {reviewsList} = this.props
+
+    if (activeReview < reviewsList.length - 1) {
+      this.setState(prevState => ({activeReview: prevState.activeReview + 1}))
+    }
+  }
+
+  renderActiveReview = review => {
+    const {imgUrl, username, companyName, description} = review
+
+    return (
+      <div className="review-container">
+        <img src={imgUrl} alt={username} />
+        <p className="username"> {username} </p>
+        <p className="company"> {companyName} </p>
+        <p className="desc"> {description}</p>
+      </div>
+    )
+  }
+
+  onClickLeftArrow = () => {
+    const {activeReview} = this.state
+
+    if (activeReview > 0) {
+      this.setState(prevState => ({activeReview: prevState.activeReview - 1}))
+    }
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    const {activeReview} = this.state
+    const currentReviewDetails = reviewsList[activeReview]
+
+    return (
+      <div className="app-container">
+        <h1 className="heading"> Reviews</h1>
+        <div className="carousel-container">
+          <button
+            type="button"
+            onClick={this.onClickLeftArrow}
+            testid="leftArrow"
+            className="arrow-button"
+            disabled={activeReview === 0}
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+              alt="left arrow"
+            />
+          </button>
+
+          {this.renderActiveReview(currentReviewDetails)}
+
+          <button
+            type="button"
+            onClick={this.onClickRightArrow}
+            testid="rightArrow"
+            className="arrow-button"
+            disabled={activeReview === reviewsList.length - 1}
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+              alt="right arrow"
+            />
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
